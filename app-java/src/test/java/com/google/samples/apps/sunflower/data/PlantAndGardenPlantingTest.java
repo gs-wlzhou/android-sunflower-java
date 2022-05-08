4725b57f89a4e77da78e415e01c6ac33e3ca2345
hiwlzhou <wlzhou@grandstream.cn>
2022-05-08 11:25:26 +0800

    first commit

---
+++ app-java/src/test/java/com/google/samples/apps/sunflower/data/PlantAndGardenPlantingTest.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2019 Shawn Wang
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.samples.apps.sunflower.data;
+
+import org.junit.Test;
+
+/**
+ * Created by Shawn Wang on 4/1/19.
+ */
+public class PlantAndGardenPlantingTest {
+
+    @Test
+    public void testDefaultValues() {
+        PlantAndGardenPlantings p = new PlantAndGardenPlantings();
+        assert p.getGardenPlantings().isEmpty();
+    }
+}
